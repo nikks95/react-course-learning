0fccb3ce3ab3403d70506895116e89fb6e9298ab
nikks95 <nikkstheroc95@gmail.com>
2022-03-06 20:31:59 +0530

    Props and using state

--- client/complete-course-guide/src/App.js
+++ client/complete-course-guide/src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
-import Person from './Person/Person';
-import './App.css';
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Nikhil", age: "26" },
+      { name: "Sahil", age: "27" },
+    ],
+  };
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>This is first page</h1>
-        <Person name="Nikhil" age="25" />
-        <Person name="Sahil"  age="26"></Person>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[0].age}
+        >
+          <p>I am CSE Student</p>
+        </Person>
       </div>
     );
   }
--- client/complete-course-guide/src/Person/Person.js
+++ client/complete-course-guide/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from "react";
 // const randomAge = () => Math.floor(Math.random()*30);
 const person = (props) => {
-    return <p>I am {props.name} and I am {props.age} years old.</p>;
-}
+  return (
+    <div>
+      <p>
+        I am {props.name} and I am {props.age} years old.
+      </p>
+      {props.children}
+    </div>
+  );
+};
 
 export default person;
