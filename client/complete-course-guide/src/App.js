ba6a13bc83af55e473c8c09f2fdff378591c8c91
nikks95 <nikkstheroc95@gmail.com>
2022-03-06 20:20:47 +0530

    Functional component props

--- client/complete-course-guide/src/App.js
+++ client/complete-course-guide/src/App.js
@@ -7,9 +7,8 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>This is first page</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Nikhil" age="25" />
+        <Person name="Sahil"  age="26"></Person>
       </div>
     );
   }
--- client/complete-course-guide/src/Person/Person.js
+++ client/complete-course-guide/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
-const randomAge = () => Math.floor(Math.random()*30);
-const person = () => {
-    return <p>I am a Person and I am {randomAge()} years old.</p>;
+// const randomAge = () => Math.floor(Math.random()*30);
+const person = (props) => {
+    return <p>I am {props.name} and I am {props.age} years old.</p>;
 }
 
 export default person;
